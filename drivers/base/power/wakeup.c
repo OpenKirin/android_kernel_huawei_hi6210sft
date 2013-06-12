1bbcad269d6641a30e1cd2f96b51ffb7d8869b6b
Julius Werner <jwerner@chromium.org>
2013-06-12 12:55:22 -0700

    PM / Sleep: Print last wakeup source on failed wakeup_count write Commit a938da0 introduced a useful little log message to tell users/debuggers which wakeup source aborted a suspend.  However, this message is only printed if the abort happens during the in-kernel suspend path (after writing /sys/power/state).
    The full specification of the /sys/power/wakeup_count facility
    allows user-space power managers to double-check if wakeups have
    already happened before it actually tries to suspend (e.g. while it
    was running user-space pre-suspend hooks), by writing the last known
    wakeup_count value to /sys/power/wakeup_count.  This patch changes
    the sysfs handler for that node to also print said log message if
    that write fails, so that we can figure out the offending wakeup
    source for both kinds of suspend aborts.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -698,7 +698,7 @@ void pm_get_active_wakeup_sources(char *pending_wakeup_source, size_t max)
 }
 EXPORT_SYMBOL_GPL(pm_get_active_wakeup_sources);
 
-static void print_active_wakeup_sources(void)
+void pm_print_active_wakeup_sources(void)
 {
 	struct wakeup_source *ws;
 	int active = 0;
@@ -727,6 +727,7 @@ static void print_active_wakeup_sources(void)
 
 	rcu_read_unlock();
 }
+EXPORT_SYMBOL_GPL(pm_print_active_wakeup_sources);
 
 /**
  * pm_wakeup_pending - Check if power transition in progress should be aborted.
@@ -753,7 +754,7 @@ bool pm_wakeup_pending(void)
 
 	if (ret) {
 		pr_info("PM: Wakeup pending, aborting suspend\n");
-		print_active_wakeup_sources();
+		pm_print_active_wakeup_sources();
 	}
 
 	return ret;
