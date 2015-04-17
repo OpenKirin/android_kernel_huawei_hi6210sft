63a65f3a32a66eecf056bc2d78d14ffebce71769
Ruchi Kandoi <kandoiruchi@google.com>
2015-04-17 16:33:29 -0700

    sched: cpufreq: Adds a field cpu_power in the task_struct cpu_power has been added to keep track of amount of power each task is consuming. cpu_power is updated whenever stime and utime are updated for a task. power is computed by taking into account the frequency at which the current core was running and the current for cpu actively running at hat frequency.
    Bug: 21498425
    Change-Id: Ic535941e7b339aab5cae9081a34049daeb44b248
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Git-commit: 94877641f6b6ea17aa335729f548eb5647db3e3e
    Git-repo: https://android.googlesource.com/kernel/msm/
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -453,4 +453,11 @@ void cpufreq_frequency_table_get_attr(struct cpufreq_frequency_table *table,
 void cpufreq_frequency_table_update_policy_cpu(struct cpufreq_policy *policy);
 
 void cpufreq_frequency_table_put_attr(unsigned int cpu);
+
+/*********************************************************************
+ *                         CPUFREQ STATS                             *
+ *********************************************************************/
+
+void acct_update_power(struct task_struct *p, cputime_t cputime);
+
 #endif /* _LINUX_CPUFREQ_H */
