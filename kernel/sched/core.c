ceee9726ae84473b02f8812e57bcaf0a59f69cdc
Christopher R. Palmer <crpalmer@gmail.com>
2016-08-04 06:39:18 -0400

    sched: Remove some small changes added by Huawei Change-Id: Ifafdf6e6a8a8ad142b3b23cc6018cf59d2d2751b

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -73,9 +73,6 @@
 #include <linux/init_task.h>
 #include <linux/binfmts.h>
 #include <linux/context_tracking.h>
-#ifdef CONFIG_HUAWEI_MSG_POLICY
-#include <huawei_platform/power/msgnotify.h>
-#endif
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
 #include <asm/irq_regs.h>
@@ -3333,10 +3330,6 @@ need_resched:
 		rq->nr_switches++;
 		rq->curr = next;
 		++*switch_count;
-
-#ifdef CONFIG_HUAWEI_MSG_POLICY
-		update_msg_stat(cpu, prev, next);
-#endif
 
 		context_switch(rq, prev, next); /* unlocks the rq */
 		/*
