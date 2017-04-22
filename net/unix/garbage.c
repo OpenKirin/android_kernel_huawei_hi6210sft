806c6c2fb11bfc183308bde637484fb9b96a94fa
Huawei-Dev <surdu_petru@yahoo.com>
2017-04-22 23:57:54 +0200

    update to 3.10.96

--- net/unix/garbage.c
+++ net/unix/garbage.c
@@ -130,6 +130,7 @@ void unix_inflight(struct file *fp)
 
 	if (s) {
 		struct unix_sock *u = unix_sk(s);
+
 		if (atomic_long_inc_return(&u->inflight) == 1) {
 			BUG_ON(!list_empty(&u->link));
 			list_add_tail(&u->link, &gc_inflight_list);
@@ -145,10 +146,12 @@ void unix_inflight(struct file *fp)
 void unix_notinflight(struct file *fp)
 {
 	struct sock *s = unix_get_socket(fp);
+
 	spin_lock(&unix_gc_lock);
 
 	if (s) {
 		struct unix_sock *u = unix_sk(s);
+
 		BUG_ON(list_empty(&u->link));
 		if (atomic_long_dec_and_test(&u->inflight))
 			list_del_init(&u->link);
