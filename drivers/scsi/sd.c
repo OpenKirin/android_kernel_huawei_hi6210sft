20be319d4c6c92f7034cb2eaaec8991c13b9ff72
HomerSp <master.homer@gmail.com>
2017-05-05 16:48:30 +0200

    disables cache synchronization when the device is write protected

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -2456,6 +2456,10 @@ sd_read_cache_type(struct scsi_disk *sdkp, unsigned char *buffer)
 			sdkp->DPOFUA = 0;
 		}
 
+		/* No cache flush allowed for write protected devices */
+		if (sdkp->WCE && sdkp->write_prot)
+			sdkp->WCE = 0;
+
 		if (sdkp->first_scan || old_wce != sdkp->WCE ||
 		    old_rcd != sdkp->RCD || old_dpofua != sdkp->DPOFUA)
 			sd_printk(KERN_NOTICE, sdkp,
