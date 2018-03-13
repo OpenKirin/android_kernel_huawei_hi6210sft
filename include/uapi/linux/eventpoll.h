7b5ead3d8eeee5c4d1f4e96198c90a01a9a9a7bd
Prasad Sodagudi <psodagud@codeaurora.org>
2018-03-13 22:43:27 +0100

    epoll: Drop struct epoll_event definition Add kernel header sanitizer check to drop struct epoll_event. This struct epoll_event is not following the POSIX standard and defining element in opaque data type.
    Change-Id: I1eecef1b64eb1fb8f0dd6999263093a453e7b9fb
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- include/uapi/linux/eventpoll.h
+++ include/uapi/linux/eventpoll.h
@@ -56,10 +56,12 @@
 #define EPOLL_PACKED
 #endif
 
+#ifdef __KERNEL__
 struct epoll_event {
 	__u32 events;
 	__u64 data;
 } EPOLL_PACKED;
+#endif /* __KERNEL__ */
 
 #ifdef CONFIG_PM_SLEEP
 static inline void ep_take_care_of_epollwakeup(struct epoll_event *epev)
